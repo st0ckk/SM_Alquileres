feea8e60aa85497a5b1acdd3f41d724fd6298744
st0ckk <anakin.salazar07@gmail.com>
2026-04-24 09:53:16 -0600

    tests2

--- SGA.Infrastructure/Services/IEmailNotificationService.cs
+++
@@ -1,8 +0,0 @@
-namespace SGA.Infrastructure.Services;
-
-public interface IEmailNotificationService
-{
-    Task SendWelcomeEmailAsync(string recipientEmail, string recipientName, CancellationToken cancellationToken = default);
-    Task SendAppointmentEmailAsync(string recipientEmail, string recipientName, DateTime fechaVisita, string propiedadInteres, CancellationToken cancellationToken = default);
-}
-
--- SGA_WEB/Models/ClienteNotificacionesViewModel.cs
+++
@@ -1,9 +0,0 @@
-using SGA.Infrastructure.Services;
-
-namespace SGA_WEB.Models;
-
-public class ClienteNotificacionesViewModel
-{
-    public IReadOnlyList<CitaResumen> Citas { get; set; } = [];
-    public ClienteMensajeInputModel Mensaje { get; set; } = new();
-}
